43909089fe71b1ad022fd4aea721a610ebe0bed9
Lucas Florio <lucasefe@gmail.com>
2012-07-12 10:00:18 -0300

    Adding simplest version.

--- init.rb
+++ init.rb
@@ -4,29 +4,22 @@ require 'readline'
 class Heroku::Command::Prompt < Heroku::Command::Base
 
   def index
-
-    stty_save = `stty -g`.chomp
-
-    cmd_prefix = ARGV.join(" ")
-
     while cmd = Readline.readline(prompt, true)
       exit if /exit/ =~ cmd
-      if cmd != ""
-        full_cmd = "heroku #{cmd} #{cmd_prefix}"
-        puts " [executing #{full_cmd}]"
-        system(full_cmd)
-      end
+      run_command cmd, opts if cmd != ""
     end
   end
 
   private
 
   def prompt
-    if ARGV.include?("--app")
-      "[ #{ARGV[ARGV.index("--app") + 1]} ] > "
-    else
-      "> "
-    end
+    "[ #{app}] > "
+  end
+
+  def opts
+    @opts ||= Heroku::Command.current_options.map do |k, v|
+      ["--#{k}", v]
+    end.flatten
   end
 end
 
