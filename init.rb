1a68f8f29c50c6b16f65002c4a0622d658c002fa
Lucas Florio <lucasefe@gmail.com>
2012-07-12 08:27:54 -0300

    Adding boilerplate code

--- init.rb
+++ init.rb
@@ -1,10 +1,31 @@
 require "fileutils"
+require 'readline'
 
 class Heroku::Command::Prompt < Heroku::Command::Base
 
   def index
-    display "Heroku Prompt: "
 
+  stty_save = `stty -g`.chomp
+
+  cmd_prefix = ARGV.join(" ")
+
+  while cmd = Readline.readline(prompt, true)
+    exit if /exit/ =~ cmd
+    if cmd != ""
+      full_cmd = "heroku #{cmd} #{cmd_prefix}"
+      puts " [executing #{full_cmd}]"
+      system(full_cmd)
+    end
+  end
+
+  private
+
+  def prompt
+    if ARGV.include?("--app")
+      "[ #{ARGV[ARGV.index("--app") + 1]} ] > "
+    else
+      "> "
+    end
   end
 end
 
